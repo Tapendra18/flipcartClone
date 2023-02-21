bce760082a7f472eebb1c42daf409b9254c2fa60
Tapendra18 <107476177+Tapendra18@users.noreply.github.com>
2023-02-21 16:21:59 +0530

    navbar component done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
+// import './App.css';
 
 //component
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 
+
+import { Box } from "@mui/material"
 function App() {
   return (
-    <div className="App">
-      <Header/>
+    <div >
+      <Header />
+      <Box style={{marginTop:54}} >
+        <Home />
+      </Box>
     </div>
   );
 }
